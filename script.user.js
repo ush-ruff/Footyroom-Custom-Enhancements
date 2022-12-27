82848a91c2be3336a5a5251b302b67f71f0e7874
ushruff <85804455+ush-ruff@users.noreply.github.com>
2022-12-27 07:44:54 +0500

    v0.2.0
    - Separated team badge data into an external json file
    - renamed some variables to avoid conflicts

--- script.user.js
+++ script.user.js
@@ -3,9 +3,10 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://footyroom.co/*
 // @grant       none
-// @version     0.1.0
+// @version     0.2.0
 // @author      ushruff
 // @description Changes all links to open in current tab, Add button onto page to scroll to top, Change some team badges with more accurate images.
+// @require     https://raw.githubusercontent.com/ush-ruff/Footyroom---Custom-Enhancements/import-require/teamBadges.json
 // ==/UserScript==
 
 // --------------------
@@ -19,158 +20,7 @@ const BADGE_ELEMENTS = {
   click: ".table-badge"
 }
 
-const TEAM_BADGES = [
-  {
-    name: "Chelsea",
-    code: 1,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/fhBITrIlbQxhVB6IjxUO6Q_96x96.png"
-  },
-  {
-    name: "Manchester United",
-    code: 2,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/udQ6ns69PctCv143h-GeYw_96x96.png"
-  },
-  {
-    name: "Manchester City",
-    code: 3,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/z44l-a0W1v5FmgPnemV6Xw_96x96.png"
-  },
-  {
-    name: "Arsenal",
-    code: 4,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/4us2nCgl6kgZc0t3hpW75Q_96x96.png"
-  },
-  {
-    name: "Aston Villa",
-    code: 5,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/uyNNelfnFvCEnsLrUL-j2Q_96x96.png"
-  },
-  {
-    name: "Everton",
-    code: 6,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/C3J47ea36cMBc4XPbp9aaA_96x96.png"
-  },
-  {
-    name: "Fulham",
-    code: 7,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/Gh7_5p3n364p4vxeM8FhNg_96x96.png"
-  },
-  {
-    name: "Liverpool",
-    code: 8,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/0iShHhASp5q1SL4JhtwJiw_96x96.png"
-  },
-  {
-    name: "Stoke City",
-    code: 10,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/T9sIbS6mr9BuRKY6y4FFLA_96x96.png"
-  },
-  {
-    name: "Sunderland",
-    code: 11,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/CQFeTfHrtxqgr3VKWtTwfA_96x96.png"
-  },
-  {
-    name: "Burnley",
-    code: 16,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/teLLSaMXim_8BA1d93sMng_96x96.png"
-  },
-  {
-    name: "Hull City",
-    code: 17,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/riiyZbb1JHuFQgZ3831jUQ_96x96.png"
-  },
-  {
-    name: "Tottenham Hotspur",
-    code: 18,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/k3Q_mKE98Dnohrcea0JFgQ_96x96.png"
-  },
-  {
-    name: "West Ham United",
-    code: 19,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/bXkiyIzsbDip3x2FFcUU3A_96x96.png"
-  },
-  {
-    name: "Wolverhampton Wanderers",
-    code: 20,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/-WjHLbBIQO9xE2e2MW3OPQ_96x96.png"
-  },
-  {
-    name: "Newcastle United",
-    code: 315,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/96CcNNQ0AYDAbssP0V9LuQ_96x96.png"
-  },
-  {
-    name: "West Bromwich Albion",
-    code: 317,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/Im2UqFKvfm3TaM7R2RYkjw_96x96.png"
-  },
-  {
-    name: "Leeds United",
-    code: 325,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/5dqfOKpjjW6EwTAx_FysKQ_96x96.png"
-  },
-  {
-    name: "Sheffield United",
-    code: 328,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/wF8AgQsssfy3_GLyVR3dSg_96x96.png"
-  },
-  {
-    name: "Leicester City",
-    code: 330,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/UDYY4FSlty6fXFBzvFfcyw_96x96.png"
-  },
-  {
-    name: "Nottingham Forrest",
-    code: 331,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/Zr6FbE-8pDH7UBpWCO8U9A_96x96.png"
-  },
-  {
-    name: "Norwich City",
-    code: 333,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/y-we_-8ySOubta5psWzDoA_96x96.png"
-  },
-  {
-    name: "Crystal Palace",
-    code: 335,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/8piQOzndGmApKYTcvyN9vA_96x96.png"
-  },
-  {
-    name: "Barnsley",
-    code: 338,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/xA0k0rZwCeUA0klmw9n9iQ_96x96.png"
-  },
-  {
-    name: "Watford",
-    code: 342,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/lHr1L31i7aJS-O8tUdHOIQ_96x96.png"
-  },
-  {
-    name: "Southampton",
-    code: 487,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/y1V4sm2SEBiWUPRIYl5rfg_96x96.png"
-  },
-  {
-    name: "AFC Bournemouth",
-    code: 500,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/IcOt-hrK04B-RlRwI3R0yA_96x96.png"
-  },
-  {
-    name: "Brentford",
-    code: 580,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/QOUce0WQBYqnkSmN6_TxGA_96x96.png"
-  },
-  {
-    name: "Brighton & Hove Albion",
-    code: 619,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/EKIe0e-ZIphOcfQAwsuEEQ_96x96.png"
-  },
-  {
-    name: "Luton Town",
-    code: 1832,
-    url: "https://ssl.gstatic.com/onebox/media/sports/logos/3e7bXAkZVEVAKQWTRQdGFw_96x96.png"
-  }
-]
+const TEAM_BADGES = teamBadges.data
 
 
 
@@ -291,9 +141,9 @@ function setupChangeBadges() {
 }
 
 function changeBadges(eventType) {
-  const teamBadges = document.querySelectorAll(BADGE_ELEMENTS[eventType])
+  const allBadgeElements = document.querySelectorAll(BADGE_ELEMENTS[eventType])
 
-  teamBadges.forEach(badge => {
+  allBadgeElements.forEach(badge => {
     const badgeNum = parseInt(badge.src.split("/").pop())
     const index = TEAM_BADGES.findIndex(element => element.code === badgeNum)
     if (index !== -1) {
